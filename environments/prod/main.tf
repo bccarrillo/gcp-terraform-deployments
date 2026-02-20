969f9a8e638b93f75f381b0ed99b389eea3e162d
bcarrillo01 <bcarrillo@kreavox.com>
2026-02-20 17:57:07 -0500

    cambio a prod

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,6 @@
 module "cloudrun" {
   source       = "../../modules/cloudrun"
-  service_name = "demo-cloudrun-prod"
+  service_name = "demo-cloudrun-prod-v2"
   region       = "us-central1"
   image        = "gcr.io/cloudrun/hello"
 }
